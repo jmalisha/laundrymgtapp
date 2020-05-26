3e291d13bd1b3593ed84253da66741579a147c7c
JamalGuyo <jamalguyo08@gmail.com>
2020-05-26 17:35:50 +0300

    style the client page

--- admin/pages/clients.php
+++ admin/pages/clients.php
@@ -21,14 +21,189 @@
     </nav>
     <div class="wrapper">
         <div class="wrapper-item">
+            <div class="item-img">
+                <div class="content">
+                    <h2 class="content-title">John Doe</h2>
+                    <p class="content-date">Phone :0732456798</p>
+                    <p class="content-date">email :john@doe.com</p>
+                    <p class="content-date">laundries completed : 23</p>
+                </div>
+            </div>
             <div class="content-wrapper">
-                <span class="content-date">Laundry completed 27</span>
-                <div class="content-title">JOHN DOE</div>
-                <div class="content-text">email address</div>
+                <div class="content-title">Laundry list</div>
+                <div class="content-text">
+                    <!--  -->
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">1.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">2.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">3.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">4.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">5.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <!--  -->
+                    <!--  -->
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">6.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">7.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">8.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">9.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">10.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <!--  -->
+                    <!--  -->
+                </div>
             </div>
         </div>
     </div>
     <br />
+    <div class="wrapper">
+        <div class="wrapper-item">
+            <div class="item-img">
+                <div class="content">
+                    <h2 class="content-title">John Doe</h2>
+                    <p class="content-date">Phone :0732456798</p>
+                    <p class="content-date">email :john@doe.com</p>
+                    <p class="content-date">laundries completed : 23</p>
+                </div>
+            </div>
+            <div class="content-wrapper">
+                <div class="content-title">Laundry list</div>
+                <div class="content-text">
+                    <!--  -->
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">1.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">2.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">3.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">4.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">5.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <!--  -->
+                    <!--  -->
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">6.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">7.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">8.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">9.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <div class="laundry">
+                        <div id="header">
+                            <div class="content-index">10.</div>
+                            <div class="content-value">12-02-2020</div>
+                        </div>
+                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
+                    </div>
+                    <!--  -->
+                    <!--  -->
+                </div>
+            </div>
+        </div>
+    </div>
     <br />
     <?php include('../footer.php') ?>
 </body>
