e91911db8c40a7c44b684bacbe597a52e1b603a9
JamalGuyo <jamalguyo08@gmail.com>
2020-05-27 20:59:46 +0300

    get client data from db and display on admin's client page

--- admin/clients.php
+++ admin/clients.php
@@ -1,3 +1,12 @@
+<?php
+require_once('../connection.php');
+// select data from db
+$userSql = "SELECT client.laundrycompleted, client.username, client.phone, client.email from client WHERE client.clientID = 1";
+$laundrySql = "SELECT laundry.laundryID, laundry.date, laundry.description FROM `laundry` WHERE laundry.clientID = 1";
+$userResult = mysqli_query($conn, $userSql);
+$laundryResult = mysqli_query($conn, $laundrySql);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,193 +30,54 @@
     </nav>
     <h1 style="text-align:center">Clients</h1>
     <br />
-    <div class="wrapper">
-        <div class="wrapper-item">
-            <div class="item-img">
-                <div class="content">
-                    <h2 class="content-title">John Doe</h2>
-                    <p class="content-date">Phone :0732456798</p>
-                    <p class="content-date">email :john@doe.com</p>
-                    <p class="content-date">laundries completed : 23</p>
-                </div>
-            </div>
+    <?php
+    if (mysqli_num_rows($userResult) > 0) {
+        while ($row = mysqli_fetch_assoc($userResult)) {
+            $laundryCompleted =  $row['laundrycompleted'];
+            $clientName =  $row['username'];
+            $clientPhone =  $row['phone'];
+            $clientEmail =  $row['email'];
+    ?>
+            <div class="wrapper">
+                <div class="wrapper-item">
+                    <div class="item-img">
+                        <div class="content">
+                            <h2 class="content-title"><?php echo $clientName ?></h2>
+                            <p class="content-date">Phone : 0<?php echo $clientPhone ?></p>
+                            <p class="content-date">email : <?php echo $clientEmail ?></p>
+                            <p class="content-date">laundries completed : <?php echo $laundryCompleted ?></p>
+                        </div>
+                    </div>
+            <?php }
+    } ?>
             <div class="content-wrapper">
                 <div class="content-title">Laundry list</div>
                 <div class="content-text">
                     <!--  -->
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">1.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">2.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">3.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">4.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">5.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <!--  -->
-                    <!--  -->
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">6.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">7.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">8.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">9.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">10.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
+                    <?php
+                    if (mysqli_num_rows($laundryResult) > 0) {
+                        while ($row = mysqli_fetch_assoc($laundryResult)) {
+                            $laundryID =  $row['laundryID'];
+                            $laundryDate =  $row['date'];
+                            $desc =  $row['description'];
+                    ?>
+                            <div class="laundry">
+                                <div id="header">
+                                    <div class="content-index"><?php echo $laundryID ?></div>
+                                    <div class="content-value"><?php echo $laundryDate ?></div>
+                                </div>
+                                <div class="content-key"><?php echo $desc ?></div>
+                            </div>
+                    <?php }
+                    } ?>
                     <!--  -->
                     <!--  -->
                 </div>
             </div>
-        </div>
-    </div>
-    <br />
-    <div class="wrapper">
-        <div class="wrapper-item">
-            <div class="item-img">
-                <div class="content">
-                    <h2 class="content-title">John Doe</h2>
-                    <p class="content-date">Phone :0732456798</p>
-                    <p class="content-date">email :john@doe.com</p>
-                    <p class="content-date">laundries completed : 23</p>
                 </div>
             </div>
-            <div class="content-wrapper">
-                <div class="content-title">Laundry list</div>
-                <div class="content-text">
-                    <!--  -->
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">1.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">2.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">3.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">4.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">5.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <!--  -->
-                    <!--  -->
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">6.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">7.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">8.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">9.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <div class="laundry">
-                        <div id="header">
-                            <div class="content-index">10.</div>
-                            <div class="content-value">12-02-2020</div>
-                        </div>
-                        <div class="content-key">5shirts, 3trousers, 7 shorts, 4 blankets, 10duvets, 20 bed sheets</div>
-                    </div>
-                    <!--  -->
-                    <!--  -->
-                </div>
-            </div>
-        </div>
-    </div>
-    <br />
-    <?php include('./footer.php') ?>
+            <br />
+            <?php include('./footer.php') ?>
 </body>
 
 </html>
