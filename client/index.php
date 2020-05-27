159df1cd06acce66c9ac1b4046694bf4439970b9
JamalGuyo <jamalguyo08@gmail.com>
2020-05-27 20:14:06 +0300

    refactor the sql select statement to display data on db

--- client/index.php
+++ client/index.php
@@ -1,35 +1,30 @@
 <?php
 require_once('../connection.php');
 
-// get all laundry data
-$laundrySql = 'SELECT laundry.date, laundry.laundrystatus, laundry.description FROM `laundry` WHERE laundry.clientID = 1';
+// select all laundry data for the client
+$laundrySql = "SELECT laundry.date, laundry.laundrystatus, laundry.description,client.laundrycompleted, admin.adminName, admin.phone FROM ((`laundry` INNER JOIN client ON client.clientID = laundry.laundryID) INNER JOIN admin ON admin.adminID = laundry.adminID) WHERE laundry.clientID = 4";
 $laundryResult = mysqli_query($conn, $laundrySql);
 ?>
+<!--  -->
 <?php include('./header.php') ?>
-<!-- <div class="clientcontainer">
-    <p>
-        No Laundry data found 😅.
-    </p>
-    <p>
-        The laundry data will be visible to you once you start your laundry service
-        with us
-    </p>
-</div> -->
+<!--  -->
 <?php
 if (mysqli_num_rows($laundryResult) > 0) {
     while ($row = mysqli_fetch_assoc($laundryResult)) {
         $laundryDate =  $row['date'];
         $laundryStatus =  $row['laundrystatus'];
         $desc =  $row['description'];
-
+        $laundryCompleted =  $row['laundrycompleted'];
+        $adminName =  $row['adminName'];
+        $adminPhone =  $row['phone'];
 ?>
         <div class="wrapper">
             <div class="wrapper-item">
                 <div class="item-img">
                     <div class="content">
-                        <h2 class="content-title" style="color: white">Laundries Completed : 23</h2>
-                        <p class="content-date" style="color: white">Served By : Lucky Dub</p>
-                        <p class="content-date" style="color: white">Phone no. : 0712345769</p>
+                        <h2 class="content-title" style="color: white">Laundries Completed : <?php echo $laundryCompleted ?></h2>
+                        <p class="content-date" style="color: white">Served By : <?php echo $adminName ?></p>
+                        <p class="content-date" style="color: white">Phone no. : 0<?php echo $adminPhone ?></p>
                     </div>
                 </div>
                 <div class="content-wrapper">
@@ -40,7 +35,17 @@ if (mysqli_num_rows($laundryResult) > 0) {
             </div>
         </div>
         <br />
-<?php
+    <?php
     }
-} ?>
+} else { ?>
+    <div class="clientcontainer">
+        <p>
+            No Laundry data found 😅.
+        </p>
+        <p>
+            The laundry data will be visible to you once you start your laundry service
+            with us
+        </p>
+    </div>
+<?php } ?>
 <?php include('./footer.php') ?>
