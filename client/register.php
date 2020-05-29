eb0b83201b635ad7f7bae067e72c341ba98c0866
JamalGuyo <jamalguyo08@gmail.com>
2020-05-29 18:01:00 +0300

    add error handling on register and login client forms

--- client/register.php
+++ client/register.php
@@ -2,6 +2,23 @@
 <div class="form-container">
     <div class="form-wrap">
         <h2>Register</h2>
+        <?php
+        if (isset($_GET["error"])) {
+            if ($_GET['error'] == 'emptyfields') {
+                echo '<p class="authError">Fill in all fields!</p>';
+            } else if ($_GET['error'] == 'invalidmailuid') {
+                echo '<p class="authError">Invalid username and email!</p>';
+            } else if ($_GET['error'] == 'invalidmail') {
+                echo '<p class="authError">Invalid email!</p>';
+            } else if ($_GET['error'] == 'invaliduid') {
+                echo '<p class="authError">Invalid username!</p>';
+            } else if ($_GET['error'] == 'passwordcheck') {
+                echo '<p class="authError">Your passwords do not match!</p>';
+            } else if ($_GET['error'] == 'usertaken') {
+                echo '<p class="authError">Username is already taken!</p>';
+            }
+        }
+        ?>
         <form class="form" method="post" action="includes/signup.inc.php">
             <input type="text" placeholder="Username" name="username" />
             <input type="email" placeholder="Email" name="email" />
