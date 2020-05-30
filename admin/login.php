52bd93762d7536bc168e9136d4de0daac3efecfd
JamalGuyo <jamalguyo08@gmail.com>
2020-05-30 11:25:21 +0300

    add login form error handling

--- admin/login.php
+++ admin/login.php
@@ -2,6 +2,17 @@
 <div class="form-container">
     <div class="form-wrap">
         <h2>Login</h2>
+        <?php
+        if (isset($_GET["error"])) {
+            if ($_GET['error'] == 'emptyfields') {
+                echo '<p class="authError">Fill in all fields!</p>';
+            } else if ($_GET['error'] == 'wrongpassword') {
+                echo '<p class="authError">Invalid password!</p>';
+            } else if ($_GET['error'] == 'nouser') {
+                echo '<p class="authError">Invalid username!</p>';
+            }
+        }
+        ?>
         <form class="form" method="GET" action="includes/login.inc.php">
             <input type="text" placeholder="Username" name="adminName" />
             <input type="password" placeholder="Password" name="password" />
